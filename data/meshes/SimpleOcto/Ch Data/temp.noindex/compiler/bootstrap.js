077f7dd35a1cd88d76e655b003dfa18a0f5c08fc
Boyd Branch <boyd.branch@gmail.com>
2019-11-16 15:54:37 +0000

    Revert "Merge branch 'master' of https://github.com/Geitenkaas/Automatic-Improvisational-Interactive-System"
    This reverts commit e8c54ab6f593a61b8354334c4fe3079bf3fd76c8 [formerly 1fd6ebdfc4388fd3556fca19381111366ce5426a], reversing
    changes made to 057e5964eb043f6ad5b49458d4647a2a87b8b8ce [formerly 3711da056934a29f9e2eb4528b5bfe29cf555c1a].
    
    
    Former-commit-id: 71a70977c6261525f6c6ce52ba1f235685e9c520
    Former-commit-id: 8a2787c2eb5dd9d9a366da9e4dd0f942a9767512

--- data/meshes/SimpleOcto/Ch Data/temp.noindex/compiler/bootstrap.js
+++
@@ -1,3 +0,0 @@
-var __bootStrapPath = "C:\\Users\\Boyd Branch\\Documents\\GitHub\\Automatic-Improvisational-Interactive-System\\data\\meshes\\SimpleOcto\\Ch Data\\temp.noindex\\compiler\\bootstrap.js";
-var __compiledScene = "C:\\Users\\Boyd Branch\\Documents\\GitHub\\Automatic-Improvisational-Interactive-System\\data\\meshes\\SimpleOcto\\Ch Data\\temp.noindex\\compiler\\scene.json";
-runScript("C:\\Program Files\\Adobe\\Adobe Character Animator CC 2019\\Support Files\\Runtime\\animal\\scripts\\js\\lib\\engine.js")
